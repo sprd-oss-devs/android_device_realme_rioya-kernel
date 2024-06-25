7dfbf6e8362fc94ae5d52cd97b7eee2dcd0ef87e
Giovanni Ricca <giovanniricca@proton.me>
2024-06-25 11:57:48 +0200

    rioya-kernel: RMX3261_11_A.91_20220819191422
    Change-Id: I0129f2d0080ebac16418f7b3d81df228629f309a

---
+++ BoardConfigKernel.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Kernel - prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)-kernel/kernel
+BOARD_MKBOOTIMG_ARGS += --dtb $(DEVICE_PATH)-kernel/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)-kernel/dtbo.img
+BOARD_INCLUDE_DTB_IN_BOOTIMG := 
